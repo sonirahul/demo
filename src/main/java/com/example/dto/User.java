fc05e353e9bfdaf4a1b1e158ef714b0ede2030b0
Rahul Soni <soni_rahul@live.com>
2017-09-10 05:12:01 +0530

    test

--- src/main/java/com/example/dto/User.java
+++ src/main/java/com/example/dto/User.java
@@ -2,9 +2,19 @@ package com.example.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class User {
 
     private String firstName;
     private String lastName;
+    @NotNull
+    private String school = null;
+
+    @Nonnull
+    public String indirectMix() {
+        String mix = null;
+        return mix;  // Noncompliant; return value is Nonnull, but null is returned.}}
+    }
 }
