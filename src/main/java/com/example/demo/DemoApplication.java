0d5097e83f99371ff12b4a7072dc2ab8a5933873
Rahul Soni <soni_rahul@live.com>
2017-09-09 22:17:17 +0530

    adding lombok

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.example.dto.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class DemoApplication {
 
 	@Value("${username}")
@@ -19,6 +22,11 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	public String testMethod() {
-		return username;
+		User user = new User();
+		user.setFirstName("Rahul");
+		user.setLastName("Soni");
+		log.debug(user.toString());
+		System.out.println(user.toString());
+		return username + " " + user.toString();
 	}
 }
